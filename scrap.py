7253bd2f31296561f51de16ed4d3eae6bcb2e076
kirankhatri-prog <officialkiran40@gmail.com>
2025-09-16 19:11:22 +0545

    Finish Project

---
+++ scrap.py
@@ -0,0 +1,48 @@
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url="http://books.toscrape.com/"
+
+
+def scrape_books(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+         print("Fail to load page")
+         return
+
+    response.encoding = response.apparent_encoding
+    # print(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all("article", class_="product_pod")
+    # print(books)
+    books_data = []  # list to store all book dicts
+
+    for book in books:
+         title = book.h3.a['title']
+     
+         price_string = book.find('p', class_='price_color').text
+         currency = price_string[0]
+         price = float(price_string[1:])
+         
+         # Add to list as dictionary
+         books_data.append({
+            "title": title,
+            "currency": currency,
+            "price": price })
+    
+       # convert to JSON string
+    json_data = json.dumps(books_data, indent=2)
+
+    # print JSON string
+    print(json_data)
+
+   
+
+       
+book = scrape_books(url)
+
+with open("book.json","w") as f:
+    import json
+    json.dumps(book, f, indent=2)
